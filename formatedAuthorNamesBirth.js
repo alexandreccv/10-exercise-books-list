689d25e2f932cd18385774e92af4b671b87619f1
Alexandre Carvalho <alex380bb@gmail.com>
2024-06-06 18:07:57 -0300

    REQ 3

--- formatedAuthorNamesBirth.js
+++ formatedAuthorNamesBirth.js
@@ -1,7 +1,6 @@
 const { books } = require('./data/library');
 
-const formatedAuthorNamesBirth = () => {
-  return books.map((book) => `${book.author.name} - ${book.author.birthYear}`);
-};
+const formatedAuthorNamesBirth = () =>
+  books.map((book) => `${book.author.name} - ${book.author.birthYear}`);
 
 module.exports = { formatedAuthorNamesBirth };
--- nameAndAge.js
+++ nameAndAge.js
@@ -1,7 +1,10 @@
-// const { books } = require('./data/library');
+const { books } = require('./data/library');
 
-const nameAndAge = () => {
-  // escreva seu código aqui
-};
+const nameAndAge = () => books.map((book) => (
+  {
+    author: book.author.name,
+    age: book.releaseYear - book.author.birthYear,
+  }
+)).sort((objAuthor, objAge) => objAuthor.age - objAge.age);
 
 module.exports = { nameAndAge };
